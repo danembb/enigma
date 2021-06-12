43fbc7bad281b94e18beb9e7e60fb458bd7cc7e5
danembb <dbrophy720@gmail.com>
2021-06-12 12:31:53 -0600

    Create #welcome method for message class

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,11 +1,19 @@
 require_relative "spec_helper"
 
 RSpec.describe Message do
+  before :each do
+    @message = Message.new
+  end
+
   describe "instantiation" do
     it "::new" do
-      message = Message.new
+      expect(@message).to be_a(Message)
+    end
+  end
 
-      expect(message).to be_a(Message)
+  describe "methods" do
+    it "#welcome can display a welcome message" do
+      expect(@message.welcome).to be_a(String)
     end
   end
 end
