a94b151c3d8901cd963254aae1357f6994f9ba4a
danembb <dbrophy720@gmail.com>
2021-06-12 12:33:16 -0600

    Create #welcome_query method for message class and refactor #welcome message

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -13,7 +13,11 @@ RSpec.describe Message do
 
   describe "methods" do
     it "#welcome can display a welcome message" do
-      expect(@message.welcome).to be_a(String)
+      expect(@message.welcome).to eq("Hello and welcome to Enigma.")
+    end
+
+    it "#welcome_query can display a welcome query" do
+      expect(@message.welcome_query).to be_a(String)
     end
   end
 end
